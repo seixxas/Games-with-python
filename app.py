4ce7c0cb01aaedb19924fe439c7d9fbd7add8b61
Guilherme Seixas <guilhermeseixasabreu@gmail.com>
2022-10-17 04:13:35 -0300

    adicionando while no programa

--- app.py
+++ app.py
@@ -7,22 +7,28 @@ print("Bem vindo ao jogo de adivinhacao")
 print("**************************")
 
 numero_secreto = 47
+total_de_tentativas = 5
 
-chute = int(input("Digite um numero: "))
+tentativa_atual = 1
 
-acertou     = chute == numero_secreto
-chute_menor = chute < numero_secreto
-chute_maior = chute > numero_secreto
+while(tentativa_atual <= total_de_tentativas):
+    print("Tentativa {} de {}".format(tentativa_atual, total_de_tentativas))
+    chute = int(input("Digite um numero: "))
 
+    acertou     = chute == numero_secreto
+    chute_menor = chute < numero_secreto
+    chute_maior = chute > numero_secreto
 
-if(acertou):
-    print("Voce acertou!")
-else:
-    if(chute_menor):
-        print("Seu chute foi menor que o numero secreto.")
-    elif(chute_maior):
-        print("Seu chute foi maior que o numero secreto.")    
-   
-
-print("Fim de jogo")    
+    if(acertou):
+        print("Voce acertou!")
+        break
+    else:
+        if(chute_menor):
+            print("Seu chute foi menor que o numero secreto.")
+        elif(chute_maior):
+             print("Seu chute foi maior que o numero secreto.")    
+    
+    tentativa_atual +=1
+    
+    
 
